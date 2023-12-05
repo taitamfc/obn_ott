1ab918c9562ded8e69e7e8a69b21267d4365fc04
taifamfc <taitam8590@gmail.com>
2023-12-05 14:58:42 +0700

    fix frontend

--- app/Http/Controllers/Website/LessonController.php
+++ app/Http/Controllers/Website/LessonController.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers\Website;
 use App\Http\Controllers\MainController;
 use Illuminate\Http\Request;
 use App\Models\Lesson;
+use App\Models\LessonStudent;
+use Illuminate\Support\Facades\Auth;
 
 class LessonController extends MainController
 {
     public function show($site_name,$id){
+
+        $student_id = Auth::guard('students')->id();
+        $check = LessonStudent::where('lesson_id',$id)->where('student_id',$student_id)->first();
+        if(!$check){
+            return redirect()->route('cms',$this->site_name);
+        }
         $item = Lesson::find($id);
         $params = [
             'item' => $item
