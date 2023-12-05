5af7c20fb71607c7a30d41b01f5a2aeb3e17fcd7
nguyenhuukhuong27102000@gmail.com <nguyenhuukhuong27102000@gmail.com>
2023-12-05 08:56:09 +0700

    fix-paypal

--- app/Http/Controllers/Website/PaymentController.php
+++ app/Http/Controllers/Website/PaymentController.php
@@ -8,6 +8,7 @@ use Srmklive\PayPal\Services\PayPal as PayPalClient;
 use App\Models\Course;
 use App\Models\StudentCourse;
 use App\Models\Transactions;
+use Illuminate\Support\Facades\Log;
 use DB;
 class PaymentController extends MainController
 {
@@ -25,7 +26,7 @@ class PaymentController extends MainController
         $response = $provider->createOrder([
             "intent" => "CAPTURE",
             "application_context" => [
-                "return_url" => route('website.success.payment',['site_name'=> $this->site_name]),
+                "return_url" => route('website.success.payment',['site_name'=> $this->site_name, 'course' => $request->course]),
                 "cancel_url" => route('website.cancel.payment',['site_name'=> $this->site_name]),
             ],
             "purchase_units" => [
@@ -47,33 +48,6 @@ class PaymentController extends MainController
                 ->route('website.cancel.payment',['site_name'=> $this->site_name])
                 ->with('error', 'Something went wrong.');
         } else {
-            try {
-                DB::beginTransaction();
-            
-                // Student And Course
-                $student_course = new StudentCourse();
-                $student_course->student_id = $this->user_id;
-                $student_course->course_id = $request->course;
-                $student_course->site_id = $this->site_id;
-                $student_course->is_complete = 0;
-                $student_course->save();
-
-                // Transaction
-                $bill = new Transactions();
-                $bill->student_id = $this->user_id;
-                $bill->course_id =isset($request->course)?$request->course:'';
-                $bill->subscription_id = isset($request->subscription)?$request->subscription:'';
-                $bill->site_id = $this->site_id;
-                $bill->price = $course->price;
-                $bill->save();
-    
-                DB::commit();
-            } catch (\Exception $e) {
-                DB::rollback();
-                return redirect()
-                ->route('website.courses.index',['site_name'=>$this->site_name])
-                ->with('error', $response['message'] ?? 'Something went wrong.');
-            }
             return redirect()
                 ->route('website.courses.index',['site_name'=>$this->site_name])
                 ->with('error', $response['message'] ?? 'Something went wrong.');
@@ -89,14 +63,47 @@ class PaymentController extends MainController
 
     public function paymentSuccess(Request $request)
     {
+        $course = Course::findOrfail($request->course);
         $provider = new PayPalClient;
         $provider->setApiCredentials(config('paypal'));
         $provider->getAccessToken();
         $response = $provider->capturePaymentOrder($request['token']);
         if (isset($response['status']) && $response['status'] == 'COMPLETED') {
-            return redirect()
+            try {
+                DB::beginTransaction();
+            
+                // Student And Course
+                $student_course = new StudentCourse();
+                $student_course->student_id = $this->user_id;
+                $student_course->course_id = $request->course;
+                $student_course->site_id = $this->site_id;
+                $student_course->is_complete = 0;
+                $student_course->save();
+
+                // Transaction
+                $bill = new Transactions();
+                $bill->student_id = $this->user_id;
+                $bill->course_id =isset($request->course)?$request->course:0e;
+                $bill->subscription_id = isset($request->subscription)?$request->subscription:0;
+                $bill->site_id = $this->site_id;
+                $bill->price = $course->price;
+                $bill->save();
+    
+                DB::commit();
+
+                return redirect()
                 ->route('website.courses.index',['site_name'=>$this->site_name])
                 ->with('success', 'Transaction complete.');
+
+            } catch (\Exception $e) {
+                DB::rollback();
+                Log::error('Bug occurred: ' . $e->getMessage());
+
+                return redirect()
+                ->route('website.courses.index',['site_name'=>$this->site_name])
+                ->with('success', 'Transaction incomplete.');
+            }
+           
         } else {
             return redirect()
                 ->route('website.courses.index',['site_name'=>$this->site_name])
