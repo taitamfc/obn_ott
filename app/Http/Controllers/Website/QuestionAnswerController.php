fd4636778e4561dbe063a94ec3fc854fca8fa0c2
hieutran01 <trandinhhieu19012002@gmail.com>
2023-12-05 14:19:29 +0700

    qas

---
+++ app/Http/Controllers/Website/QuestionAnswerController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Website;
+
+use App\Http\Controllers\MainController;
+use App\Http\Requests\StoreQasRequest;
+use Illuminate\Http\Request;
+use App\Models\QuestionAnswer;
+use Illuminate\Support\Facades\Auth;
+
+class QuestionAnswerController extends MainController
+{
+    public function index()
+    {
+        $student = Auth::guard('students')->user();
+        $items = QuestionAnswer::where('student_id', $student->id)->orderBy('created_at', 'desc')->get();
+        return view('website.dashboards.qas.index', ['items' => $items]);
+    }
+
+    public function create()
+    {
+        return view('website.dashboards.qas.create');
+    }
+
+    public function store(StoreQasRequest $request) {
+        $student = Auth::guard('students')->user();
+        $items = new QuestionAnswer();
+        $items->title = $request->input('title');
+        $items->question = $request->input('question');
+        $items->student_id = $student->id;
+        $items->save();
+        return redirect()->route('website.q-a',['site_name'=>$this->site_name])->with('success', 'Question-Answer created successfully');
+    }
+
+    public function show($site_name,$id)
+    {
+        $item = QuestionAnswer::find($id);
+        return view('website.dashboards.qas.show', ['item' => $item]);
+    }
+}
