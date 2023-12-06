054fcef02bf55e6155b9a8560cfebceec78e2e89
hieutran01 <trandinhhieu19012002@gmail.com>
2023-12-06 09:28:54 +0700

    fix-layout

--- resources/views/website/layouts/master.blade.php
+++ resources/views/website/layouts/master.blade.php
@@ -36,17 +36,17 @@
     <script src="{{ asset('website/js/bootstrap.min.js')}}"></script>
     <script src="{{ asset('website/js/isotope.pkgd.min.js')}}"></script>
     <script src="{{ asset('website/js/slick.min.js')}}"></script>
-    <!-- <script src="{{ asset('website/js/jquery.meanmenu.min.js')}}"></script> -->
-    <!-- <script src="{{ asset('website/js/ajax-form.js')}}"></script> -->
+    <script src="{{ asset('website/js/jquery.meanmenu.min.js')}}"></script>
+    <script src="{{ asset('website/js/ajax-form.js')}}"></script>
     <script src="{{ asset('website/js/wow.min.js')}}"></script>
-    <!-- <script src="{{ asset('website/js/jquery.scrollUp.min.js')}}"></script> -->
-    <!-- <script src="{{ asset('website/js/imagesloaded.pkgd.min.js')}}"></script> -->
+    <script src="{{ asset('website/js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{ asset('website/js/imagesloaded.pkgd.min.js')}}"></script>
     <script src="{{ asset('website/js/jquery.magnific-popup.min.js')}}"></script>
-    <!-- <script src="{{ asset('website/js/waypoints.min.js')}}"></script> -->
+    <script src="{{ asset('website/js/waypoints.min.js')}}"></script>
     <script src="{{ asset('website/js/jquery.counterup.min.js')}}"></script>
     <script src="{{ asset('website/js/plugins.js')}}"></script>
     <script src="{{ asset('website/js/swiper-bundle.min.js')}}"></script>
-    <!-- <script src="{{ asset('website/js/main.js')}}"></script> -->
+    <script src="{{ asset('website/js/main.js')}}"></script>
     <script src="{{ asset('website/js/custom.js')}}"></script>
     <script>
     $(document).ready(function() {
