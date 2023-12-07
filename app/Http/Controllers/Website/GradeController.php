86d5bc92fcd6d9d9e5fd25f9a895423122f623d0
nguyenhuukhuong27102000@gmail.com <nguyenhuukhuong27102000@gmail.com>
2023-12-07 21:53:40 +0700

    fix-detail-grade

--- app/Http/Controllers/Website/GradeController.php
+++ app/Http/Controllers/Website/GradeController.php
@@ -12,17 +12,23 @@ use App\Models\Impression;
 class GradeController extends MainController
 {
     public function show($site_id,$id){
-        $item =  Grade::find($id);
-        Impression::increase($this->site_id,$id);
-        Visitor::increase($this->site_id,$id);
-        $items = Subject::where('site_id',$this->site_id)
-                            ->where('grade_id',$id)
-                            ->where('status',Subject::ACTIVE)
-                            ->get();
-        $params = [
-            'item'  => $item,
-            'items' => $items,
-        ];
-        return view('website.grades.show',$params);
+        try {
+            $item =  Grade::findOrfail($id);
+            Impression::increase($this->site_id,$id);
+            Visitor::increase($this->site_id,$id);
+            $items = Subject::where('site_id',$this->site_id)
+                                ->where('grade_id',$id)
+                                ->where('status',Subject::ACTIVE)
+                                ->get();
+            $params = [
+                'item'  => $item,
+                'items' => $items,
+            ];
+            return view('website.grades.show',$params);
+        } catch (ModelNotFoundExeption $e) {
+            abort(404);
+        } catch (Exception $e){
+            Log::error("Bug error : ". $e->getMessage());
+        }
     }
 }
