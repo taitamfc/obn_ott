a2f9e450c374be7c760f787221b93681cbf4d292
taifamfc <taitam8590@gmail.com>
2023-12-19 09:57:40 +0700

    fix email

--- resources/views/admin/includes/mailMember.blade.php
+++ resources/views/admin/includes/mailMember.blade.php
@@ -1,9 +1,8 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 <div>
     <h3>{{__('login.hi')}}, {{ $data['name'] }}</h3>
     <p>Your account has been created.</p>
-    <p>Click link to page.</p>
-    <a href="{{ $data['login_link'] }}">Home Page</a>
+    <p>Click link to page: <a href="{{ $data['site_link_login'] }}">{{ $data['site_title'] }}</a></p>
+    
     <b>Your email: {{ $data['email'] }}<br></b>
     <b>Your password: {{ $data['password'] }}<br></b>
     <p>{{__('login.if-you-do not-take-any-action')}}, 
