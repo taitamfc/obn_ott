4a3d374361bf317579aa6aa24825eb4febf15d99
taifamfc <taitam8590@gmail.com>
2023-12-25 12:49:36 +0700

    tam fix ott 25/12

--- resources/views/page/plans/BuyPlan.blade.php
+++ resources/views/page/plans/BuyPlan.blade.php
@@ -1,6 +1,6 @@
 @extends('page.layouts.master')
 @section('title')
-    {{ __('admin-sidebar.plan') }}
+{{ __('admin-sidebar.plan') }}
 @endsection
 @section('content')
 <div class="checkoutarea sp_bottom_100 sp_top_100">
@@ -11,51 +11,61 @@
                 <div class="col-xl-6 col-lg-6 col-md-12">
                     <div class="checkoutarea__billing">
                         <div class="checkoutarea__billing__heading">
-                            <h2>Billing Details</h2>
+                            <h2>SIGN UP</h2>
                         </div>
                         <div class="checkoutarea__billing__form">
                             <div class="row">
                                 <div class="col-xl-12">
                                     <div class="checkoutarea__inputbox">
-                                        <label for="first__name">Full Name
-                                            *</label>
-                                        <input type="text" id="first__name"
-                                            name="nameuser" class="info"
-                                            placeholder="Full Name">
-                                            @if($errors->has('nameuser'))
-                                                <div style="color:red;font-size: 12px;" class="error-message pd-0">{{ $errors->first('nameuser') }}</div>
-                                            @endif
+                                        <label for="first__name">Full Name*</label>
+                                        <input type="text" id="first__name" name="nameuser" class="mb-1"
+                                            placeholder="Full Name" value="{{ old('nameuser') }}">
+                                        @if($errors->has('nameuser'))
+                                        <div class="text-danger error-message pd-0">
+                                            {{ $errors->first('nameuser') }}
+                                        </div>
+                                        @endif
                                     </div>
                                 </div>
-    
+
                                 <div class="col-xl-12">
                                     <div class="checkoutarea__inputbox">
-                                        <label for="email__address">Email
-                                            Address*</label>
-                                        <input type="text" id="email__address"
-                                            name="email" class="info"
-                                            placeholder="Your email">
+                                        <label for="email__address">Email Address*</label>
+                                        <input type="text" id="email__address" name="email" class="mb-1"
+                                            placeholder="Your email" value="{{ old('email') }}">
                                         @if($errors->has('email'))
-                                            <div style="color:red;font-size: 12px;" class="error-message pd-0">{{ $errors->first('email') }}</div>
+                                        <div  class="text-danger error-message pd-0">
+                                            {{ $errors->first('email') }}
+                                        </div>
                                         @endif
                                     </div>
                                 </div>
                                 <div class="col-xl-12">
                                     <div class="checkoutarea__inputbox">
-                                        <label for="phone__number">Phone
-                                            Number*</label>
-                                        <input type="text" id="phone__number"
-                                            name="phone" class="info"
-                                            placeholder="Phone Number">
+                                        <label for="password">Password*</label>
+                                        <input type="text" id="password" name="password" class="mb-1"
+                                            placeholder="Password" value="{{ old('password') }}">
+                                        @if($errors->has('password'))
+                                        <div  class="text-danger error-message pd-0">
+                                            {{ $errors->first('password') }}</div>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="col-xl-12">
+                                    <div class="checkoutarea__inputbox">
+                                        <label for="phone__number">Phone Number*</label>
+                                        <input type="text" id="phone__number" name="phone" class="mb-1"
+                                            placeholder="Phone Number" value="{{ old('phone') }}">
                                         @if($errors->has('phone'))
-                                            <div style="color:red;font-size: 12px;" class="error-message pd-0">{{ $errors->first('phone') }}</div>
+                                        <div  class="text-danger error-message pd-0">
+                                            {{ $errors->first('phone') }}</div>
                                         @endif
                                         <input style="display: none;" type="text" name="plan_id" value="{{$item->id}}">
                                         <input style="display: none;" type="text" name="price" value="{{$item->price}}">
                                     </div>
                                 </div>
-    
-    
+
+
                             </div>
                         </div>
                     </div>
@@ -95,14 +105,12 @@
                             <div class="checkoutarea__payment__toggle">
                                 <div class="checkoutarea__payment__total">
                                     <div class="checkoutarea__payment__type">
-                                        <input type="radio" value="paypal" id="pay-toggle04"
-                                            name="pay" checked>
+                                        <input type="radio" value="paypal" id="pay-toggle04" name="pay" checked>
                                         <label for="pay-toggle04">Paypal</label>
                                     </div>
                                 </div>
                                 <div class="checkoutarea__payment__input__box">
-                                    <button class="default__button" type="submit">Place
-                                        order</button>
+                                    <button class="default__button" type="submit">Register</button>
                                 </div>
                             </div>
                         </div>
@@ -115,4 +123,10 @@
 @endsection
 
 @section('footer')
+<script src="{{ asset('website/js/jquery.inputmask.bundle.js') }}"></script>
+<script>
+    jQuery(document).ready( function(){
+        $('#phone__number').inputmask("(999) 999-9999");
+    });
+</script>
 @endsection
